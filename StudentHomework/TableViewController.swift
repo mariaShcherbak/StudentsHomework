3d6a0ed778f38656db8bfdf2ce47d35d13dc2430
mariaShcherbak <maria.shcherbakk@gmail.com>
2021-08-30 13:23:26 +0300

    add error

--- StudentHomework/TableViewController.swift
+++ StudentHomework/TableViewController.swift
@@ -7,24 +7,28 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
 let idCell = "studentCell"
     
     @IBOutlet weak var tableView: UITableView!
-    var students = [Student(name: "Olivia", surname: "King", image:              UIImage(named: "Olivia King.png")!),
-                               Student(name: "Freddy", surname: "Walker", image: UIImage(named: "Freddy Walker.png")!),
-                               Student(name: "Liam", surname: "Evans", image: UIImage(named: "Liam Evans.png")!),
-                               Student(name: "Jacob", surname: "Roberts", image: UIImage(named: "Jacob Roberts.png")!),
-                               Student(name: "Ethan", surname: "Mills", image: UIImage(named: "Ethan Mills.png")!),
-                               Student(name: "Alexander", surname: "Lewis", image: UIImage(named: "Alexander Lewis.png")!),
-                               Student(name: "Valery", surname: "Morgan", image: UIImage(named: "Valery Morgan.png")!),
-                               Student(name: "Vanessa", surname: "Florence", image: UIImage(named: "Vanessa Florence.png")!),
-                               Student(name: "Wendy", surname: "Campbell", image: UIImage(named: "Wendy Campbell.png")!),
-                               Student(name: "Amelia", surname: "Smith", image: UIImage(named: "Amelia Smith.png")!)]
+    var students = [Student(name: "Olivia", bio: "born in 1992", image:              UIImage(named: "OliviaKing.png")!),
+                    Student(name: "Freddy", bio: "born in 1991", image: UIImage(named: "FreddyWalker.png")!),
+                               Student(name: "Liam", bio: "born in 1993", image: UIImage(named: "LiamEvans.png")!),
+                               Student(name: "Jacob", bio: "born in 1991", image: UIImage(named: "JacobRoberts.png")!),
+                               Student(name: "Ethan", bio: "born in 1992", image: UIImage(named: "EthanMills.png")!),
+                               Student(name: "Alexander", bio: "born in 1993", image: UIImage(named: "AlexanderLewis.png")!),
+                               Student(name: "Valery", bio: "born in 1992", image: UIImage(named: "ValeryMorgan.png")!),
+                               Student(name: "Vanessa", bio: "born in 1992", image: UIImage(named: "VanessaFlorence.png")!),
+                               Student(name: "Wendy", bio: "born in 1993", image: UIImage(named: "WendyCampbell.png")!),
+                               Student(name: "Amelia", bio: "born in 1993", image: UIImage(named: "AmeliaSmith.png")!)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        
+        let myButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addStudent))
+        navigationItem.rightBarButtonItem = myButton
         }
-        
+    @objc func addStudent () {
+        let newVC = storyboard?.instantiateViewController(withIdentifier: "TextFieldViewController")
+       navigationController?.pushViewController(newVC!, animated: true)
+    }
     
     
     //количество секций
@@ -54,8 +58,8 @@ let idCell = "studentCell"
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .value1, reuseIdentifier: idCell)
         let model = students[indexPath.row]
-        cell.textLabel?.text = model.title
-        //cell.detailTextLabel?.text = model.surname
+        cell.textLabel?.text = model.name
+        cell.detailTextLabel?.text = model.bio
         cell.imageView?.image = model.image
         return cell
     }
@@ -63,17 +67,18 @@ let idCell = "studentCell"
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("\(indexPath.row)") // нажатие на ячейку
     }
-    
-
+   /*
+    @IBAction func add(_ sender: Any) {
+        let newVC = storyboard?.instantiateViewController(withIdentifier: "TextViewController")
+       navigationController?.pushViewController(newVC!, animated: true)
+    }
+   */
 }
 
 
 struct Student {
     var name: String
-    var surname: String
-    var title: String {
-    "\(name) \(surname)"
-    }
+    var bio: String
     var image: UIImage
 }
 
---
+++ StudentHomework/TextFieldViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TextViewController.swift
+//  StudentHomework
+//
+//  Created by Tanya on 25.08.2021.
+//
+
+import UIKit
+
+class TextFieldViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var bioTextField: UITextField!
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
